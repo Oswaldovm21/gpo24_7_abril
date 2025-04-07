0c5547f0e99c36e8757aa16a04c6ccdb78ddb312
Jose Oswaldo Vega Montes <lizethtrrs06@gmail.com>
2025-04-07 15:31:47 -0600

    se termino el prgrama

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
 class Cuadrado:
-    pass
+    def __init__(self,lado):
+        self.lado = lado
+
+    def calcular_area(self):
+        return self.lado ** 2
+
+cuadrado = Cuadrado(10)
+print(cuadrado.calcular_area())
